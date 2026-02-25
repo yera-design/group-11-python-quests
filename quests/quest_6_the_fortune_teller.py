51f1b8e34e4f3f66f3dd368ea112b031b206fb1c
blewis-bump <b.lewis@alustudent.com>
2026-02-25 13:33:07 -0800

    add files

---
+++ quests/quest_6_the_fortune_teller.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+user_name = input("what is your name?")
+user_quest = input("what is your question?")
+print(f" Welcome, {user_name}! your {user_quest} ticket has been received now.")
---
+++ quests/quest_9_the_greedy_goblin.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+friends_fraction = 27//4
+remainder = 27%4
+print(f"friends: {friends_fraction} gold each.")
+print(f"remainder: {remainder} gold.")
